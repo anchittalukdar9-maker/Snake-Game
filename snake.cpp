a827b532a7d026d87f866df74234756774d4ad49
Anchit Talukdar <anchittalukdar9@gmail.com>
2026-05-23 00:58:02 +0530

    initial board and snake head

---
+++ snake.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+char direction;
+int height = 10, width = 20;
+int x = (width / 2) - 2, y = (height / 2) - 2;
+
+void draw()
+{
+    for (int i = 0; i < width; i++)
+    { // draws the top border
+        cout << "#";
+    }
+    cout << endl;
+    for (int j = 0; j < height - 2; j++)
+    {
+        cout << "#";
+        for (int k = 0; k < width - 2; k++)
+        {
+            if (k == x && j == y)
+            {
+                cout << "O";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << "#" << endl;
+    }
+    for (int i = 0; i < width; i++)
+    { // draws the bottom border
+        cout << "#";
+    }
+    cout << endl;
+}
+int main()
+{
+    while (true)
+    {
+        system("cls");
+
+        draw();
+        cout << "Enter direction (W/A/S/D): ";
+        cin >> direction;
+        if (direction == 'w' || direction == 'W')
+        {
+            y--;
+        }
+        else if (direction == 's' || direction == 'S')
+        {
+            y++;
+        }
+        else if (direction == 'a' || direction == 'A')
+        {
+            x--;
+        }
+        else if (direction == 'd' || direction == 'D')
+        {
+            x++;
+        }
+        
+    }
+    return 0;
+}
