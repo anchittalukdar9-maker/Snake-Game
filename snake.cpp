16d3e4eff6a30085018c78e2de1530a55d1b288c
Anchit Talukdar <anchittalukdar9@gmail.com>
2026-05-25 14:32:01 +0530

    implemented food and size growing system

--- snake.cpp
+++ snake.cpp
@@ -5,7 +5,11 @@ using namespace std;
 char direction;
 int height = 10, width = 20;
 int x = (width / 2) - 2, y = (height / 2) - 2;
-    //food spawning system
+//snake body and tail system
+const int maxtail = 100;
+int tailx[maxtail], taily[maxtail];
+int taillength=0;
+//food spawning system
    
 int fx;
 int fy;
@@ -29,8 +33,17 @@ void draw()
                 cout<<"@";
             }
             else
-            {
-                cout << " ";
+            {bool printed  = false;
+                for(int i=0; i<taillength;i++){
+                    if(tailx[i]==k&&taily[i]==j){
+                        cout<<"o";
+                        printed = true;
+                        break;
+                    }
+                }
+                if(!printed){
+                    cout << " ";
+                }
             }
         }
         cout << "#" << endl;
@@ -58,6 +71,13 @@ int main()
         draw();
         cout << "Enter direction (W/A/S/D): ";
         cin >> direction;
+        //body coordinate setting
+        for(int i=taillength-1;i>0;i--){
+            tailx[i] = tailx[i-1];
+            taily[i] = taily[i-1];
+        }
+        tailx[0] = x;
+        taily[0] = y;
         if (direction == 'w' || direction == 'W')
         {
             y--;
@@ -73,6 +93,18 @@ int main()
         else if (direction == 'd' || direction == 'D')
         {
             x++;
+        }
+        //food capture and new generation
+        if(x==fx&&y==fy){
+            taillength++;
+            tailx[taillength-1] = x;
+            taily[taillength-1] = y;
+            fx = rand() % (width - 2);
+            fy = rand() % (height -2);
+            while(fx==x&&fy==y){
+                fx = rand() % (width - 2);
+                fy = rand() % (height -2);
+            }
         }
         // check for wall collision
         if(x<0||x>=width-2||y<0||y>=height-2){
