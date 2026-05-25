c7d5566de97446ddf03559ce5c3258ba031952a6
Anchit Talukdar <anchittalukdar9@gmail.com>
2026-05-26 00:05:14 +0530

    completed playable snake game

--- snake.cpp
+++ snake.cpp
@@ -4,15 +4,15 @@
 #include <conio.h>
 #include <windows.h>
 using namespace std;
-char direction;
+char currentDirection = 'd';
 int height = 10, width = 20;
 int x = (width / 2) - 2, y = (height / 2) - 2;
-//snake body and tail system
+// snake body and tail system
 const int maxtail = 100;
 int tailx[maxtail], taily[maxtail];
-int taillength=0;
-//food spawning system
-   
+int taillength = 0;
+// food spawning system
+
 int fx;
 int fy;
 
@@ -31,19 +31,25 @@ void draw()
             if (k == x && j == y)
             {
                 cout << "O";
-            }else if(k==fx&&j==fy){
-                cout<<"@";
+            }
+            else if (k == fx && j == fy)
+            {
+                cout << "@";
             }
             else
-            {bool printed  = false;
-                for(int i=0; i<taillength;i++){
-                    if(tailx[i]==k&&taily[i]==j){
-                        cout<<"o";
+            {
+                bool printed = false;
+                for (int i = 0; i < taillength; i++)
+                {
+                    if (tailx[i] == k && taily[i] == j)
+                    {
+                        cout << "o";
                         printed = true;
                         break;
                     }
                 }
-                if(!printed){
+                if (!printed)
+                {
                     cout << " ";
                 }
             }
@@ -60,66 +66,98 @@ int main()
 {
     srand(time(0));
     fx = rand() % (width - 2);
-    fy = rand() % (height -2);
-    //overlap of food and snake check
-    while(fx==x&&fy==y){
+    fy = rand() % (height - 2);
+    // overlap of food and snake check
+    while (fx == x && fy == y)
+    {
         fx = rand() % (width - 2);
-        fy = rand() % (height -2);
+        fy = rand() % (height - 2);
     }
     while (true)
     {
         system("cls");
+        cout.flush();
 
         draw();
         if (_kbhit())
         {
-            direction =_getch();
+            char newDirection = _getch();
+
+            if ((newDirection == 'w' || newDirection == 'W') &&
+                currentDirection != 's')
+            {
+                currentDirection = 'w';
+            }
+
+            else if ((newDirection == 's' || newDirection == 'S') &&
+                     currentDirection != 'w')
+            {
+                currentDirection = 's';
+            }
+
+            else if ((newDirection == 'a' || newDirection == 'A') &&
+                     currentDirection != 'd')
+            {
+                currentDirection = 'a';
+            }
+
+            else if ((newDirection == 'd' || newDirection == 'D') &&
+                     currentDirection != 'a')
+            {
+                currentDirection = 'd';
+            }
         }
-        //body coordinate setting
-        for(int i=taillength-1;i>0;i--){
-            tailx[i] = tailx[i-1];
-            taily[i] = taily[i-1];
+        // body coordinate setting
+        for (int i = taillength - 1; i > 0; i--)
+        {
+            tailx[i] = tailx[i - 1];
+            taily[i] = taily[i - 1];
         }
         tailx[0] = x;
         taily[0] = y;
-        if (direction == 'w' || direction == 'W')
+        if (currentDirection == 'w')
         {
             y--;
         }
-        else if (direction == 's' || direction == 'S')
+        else if (currentDirection == 's')
         {
             y++;
         }
-        else if (direction == 'a' || direction == 'A')
+        else if (currentDirection == 'a')
         {
             x--;
         }
-        else if (direction == 'd' || direction == 'D')
+        else if (currentDirection == 'd')
         {
             x++;
         }
-        //food capture and new generation
-        if(x==fx&&y==fy){
+        // food capture and new generation
+        if (x == fx && y == fy)
+        {
             taillength++;
             fx = rand() % (width - 2);
-            fy = rand() % (height -2);
-            while(fx==x&&fy==y){
+            fy = rand() % (height - 2);
+            while (fx == x && fy == y)
+            {
                 fx = rand() % (width - 2);
-                fy = rand() % (height -2);
+                fy = rand() % (height - 2);
             }
         }
         // check for wall and body collision
-        if(x<0||x>=width-2||y<0||y>=height-2){
-            cout<<"Game Over!"<<endl;
+        if (x < 0 || x >= width - 2 || y < 0 || y >= height - 2)
+        {
+            cout << "Game Over!" << endl;
             break;
         }
-        for(int i=1;i<taillength;i++){
-            if(tailx[i]==x&&taily[i]==y){
-                cout<<"Game Over!"<<endl;
+        for (int i = 1; i < taillength; i++)
+        {
+            if (tailx[i] == x && taily[i] == y)
+            {
+                cout << "Game Over!" << endl;
                 break;
             }
         }
-        Sleep(200);//game speed control
+        Sleep(200); // game speed control
     }
     return 0;
 }
